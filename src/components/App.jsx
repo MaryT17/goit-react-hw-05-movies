afdbd62272275e3b2c2f4320a76653cd3266483e
MaryT17 <tmary11996@gmail.com>
2024-08-05 01:05:10 +0800

    update1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
+import React, { Suspense, lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import SearchBar from './components/SearchBar/SearchBar';
+
+const Home = lazy(() => import('./components/Home/Home'));
+const Movies = lazy(() => import('./components/Movies/Movies'));
+const Cast = lazy(() => import('./components/Cast/Cast'));
+const MovieDetails = lazy(() =>
+  import('./components/MovieDetails/MovieDetails')
+);
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <div>
+        <SearchBar />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId/*" element={<MovieDetails />} />
+            <Route path="/movies/:movieId/cast/*" element={<Cast />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
 };
+
+export default App;
